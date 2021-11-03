a24febfcf64deedcc37db3a7f9d2e3d6771b40aa
gabrieljaegerde <gabrieljaeger1@gmail.com>
2021-11-03 14:30:25 +0100

    ensuring no other collections added

--- tools/remarkStorageAdapter.ts
+++ tools/remarkStorageAdapter.ts
@@ -124,11 +124,12 @@ export class RemarkStorageAdapter implements IConsolidatorAdapter {
   }
 
   public async updateCollectionMint(collection: CollectionConsolidated): Promise<CollectionConsolidated> {
-    await this.db.read();
-    this.db.data.collections.push(collection);
-    await this.db.write();
-    const collectionDb = await this.getCollectionById(collection.id);
-    return collectionDb;
+    // await this.db.read();
+    // this.db.data.collections.push(collection);
+    // await this.db.write();
+    // const collectionDb = await this.getCollectionById(collection.id);
+    // return collectionDb;
+    return null;
   }
 
   public async updateCollectionIssuer(
